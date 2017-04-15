9a4f3122a84edbf0050bfa691b84db1324184fef
QuiqueJPG <jesuse.epg@gmail.com>
2017-04-15 16:30:08 -0400

    ID de sesion

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-var socket = io.connect ('http://192.168.56.1:6677', {'forceNew': true});//se conecta en dicha direccion/puerto
+var socket = io.connect ('http://localhost:6677/', {'forceNew': true});//se conecta en dicha direccion/puerto
 
 var tablero = [[null, null, null], [null, null, null], [null, null, null]];
 
@@ -6,20 +6,38 @@ var con = 0;
 
 var jugadores=0;
 
+var gameParams = {
+    userName:"",
+    wins:0,
+    losses:0,
+    sessId:"",
+    stalemates:0
+};
+
+var clientId;
+
+
 /*socket.on('messages', function(data){ //recibe lo que se emite desde el servidor
-	console.log(data);
-	render(data);
+  console.log(data);
+  render(data);
 });*/
 
+socket.on('connect', function () {
+      clientId = socket.io.engine.id;
+      initializeGameParams();
+      console.log("ID Assigned - " + clientId);
+
+});
+
 socket.on('tablero', function(data){ //recibe lo que se emite desde el servidor
-	console.log('Tablero recibido desde el server: ' + data.tablero);
-	tablero = data.tablero;
-	con = data.con;
-	var boton = data.boton;
-	//tablero = data;
-	console.log('Tablero actualizado con info del server: ' + tablero + ' boton desde el server: ' + boton);
-	if(boton!= undefined)
-		render(con, boton);
+  console.log('Tablero recibido desde el server: ' + data.tablero);
+  tablero = data.tablero;
+  con = data.con;
+  var boton = data.boton;
+  //tablero = data;
+  console.log('Tablero actualizado con info del server: ' + tablero + ' boton desde el server: ' + boton);
+  if(boton!= undefined)
+    render(con, boton);
 });
 
 socket.on('jugador', function(data){
@@ -30,94 +48,63 @@ socket.on('jugador', function(data){
 
 function render (cont, data){
 
-	boton = document.getElementById(data);
-	console.log('Boton en render: ' + data);
-	if(boton.className=="buttons")
- 		{
-			//  alert(boton.name);
-  			boton.className="clicked";
-  			var text;
-  			if((cont-1)%2==0)
-  				{
-   					text="X";
-  				}
-  			else
-  				{
-   					text="O";
-  				}
-  			boton.value=text;
-  			//alert(boton.value);
-  			boton.appendChild(document.createTextNode(text));
+  boton = document.getElementById(data);
+  console.log('Boton en render: ' + data);
+  if(boton.className=="buttons")
+    {
+      //  alert(boton.name);
+        boton.className="clicked";
+        var text;
+        if((cont-1)%2==0)
+          {
+            text="X";
+          }
+        else
+          {
+            text="O";
+          }
+        boton.value=text;
+        //alert(boton.value);
+        boton.appendChild(document.createTextNode(text));
  
- 		}	
- 		else
- 			{
-  				//alert("Yei!!!");
-  				//alert(boton.value);
- 			}
+    } 
+    else
+      {
+          //alert("Yei!!!");
+          //alert(boton.value);
+      }
 
 };
-
-/*function render (data){
-
-	var html = data.map(function(message, index){
-		return (`
-			<div class="message"> 
-				<strong>${message.nickname}</strong> dice:
-				<p>${message.text}</p>
-			</div>
-
-		`);
-
-
-	}).join(' ');
-
-	var div_msgs = document.getElementById('messages');
-	div_msgs.innerHTML = html;
-	div_msgs.scrollTop = div_msgs.scrollHeight;
-
-};*/
-
-/*function addMessage(e){
-	var message = {
-		nickname: document.getElementById('nickname').value,
-		text: document.getElementById('text').value
-	};
-
-	document.getElementById('nickname').style.display = 'none';
-	socket.emit('add-message', message);
-	return false;
-}*/
 
 
 function actions(boton, x, y)
 {
-  	console.log(x);
-  	//console.log(boton);
-  	console.log(boton.id);
-  	console.log(y);
-  	var jugadorActual;
- 	  if(jugadores==2)
+    console.log(x);
+    //console.log(boton);
+    console.log(boton.id);
+    console.log(y);
+    var jugadorActual;
+    if(jugadores==2)
     {
         if(boton.className=="buttons")
-     		{
-    		  	//  alert(boton.name);
-      			boton.className="clicked";
-      			var text;
-      			if(con%2==0)
-      			{
-       					text="X";
-       					jugadorActual=1;
-      			}
-      			else
-      			{
-       		 		  text="O";
-       					jugadorActual=0;
-      			}
-      			boton.value=text;
-      			//alert(boton.value);
-      			boton.appendChild(document.createTextNode(text));
-      			con++;
+        {
+            //  alert(boton.name);
+            boton.className="clicked";
+            var text;
+            if(con%2==0)
+            {
+                text="X";
+                jugadorActual=1;
+            }
+            else
+            {
+                text="O";
+                jugadorActual=0;
+            }
+            boton.value=text;
+            //alert(boton.value);
+            boton.appendChild(document.createTextNode(text));
+            con++;
 
             tablero[x][y]=jugadorActual;
         var boton = boton.id;
@@ -125,13 +112,153 @@ function actions(boton, x, y)
         socket.emit('movimiento', {tablero, con, boton});
         //socket.emit('movimiento', tablero);
      
-     		}	
-     		else
-     		{
-      			//alert("Yei!!!");
-      			alert('Movimiento no valido');
-     		}
+        } 
+        else
+        {
+            //alert("Yei!!!");
+            alert('Movimiento no valido');
+        }
     }
     else
       alert('Esperando por un segundo jugador!');
+}
+
+
+function initializeGameParams(){
+
+        var gameCookie = getCookie("tttGameParams");
+
+        if (gameCookie!="") {
+            getGameParams(gameCookie);
+            gameParams.sessId=clientId;
+
+        }else
+        {
+            gameParams.userName=clientId;
+            gameParams.sessId=clientId;
+        }
+
+
+       /* $('#updateName').bind("click", function () {
+            var user = prompt("Add a Name","");
+            if (user!=null) {
+                gameParams.userName=user;
+                saveParams();
+                updateDisplay();
+                socket.emit("updatePlayerName",{"name": user});
+            }
+
+
+        });*/
+
+        /*$('#viewButton').bind("click", function () {
+            if (!logViewShow){
+                $('#archiveMessages').slideDown(500);
+                $('#viewButton').empty().append("Close");
+                logViewShow=true;
+            }else
+            {
+                $('#archiveMessages').slideUp(500);
+                $('#viewButton').empty().append("All Logs");
+                logViewShow=false;
+            }
+
+
+        });*/
+
+
+        /*$('#swapIcons').bind("click", function() {
+
+            if (playerIcon=="X".trim()) {
+                playerIcon="O";
+                oppPlayerIcon="X";
+            }else
+            {
+                playerIcon="X";
+                oppPlayerIcon="O";
+            }
+            $("#myIcon").empty().append(playerIcon);
+            $("#oppIcon").empty().append(oppPlayerIcon);
+        });*/
+
+        saveParams();
+        updateDisplay();
+    }
+
+/**
+     * Update Display with Player info.
+     *
+     */
+function updateDisplay()
+{
+  //$("#playerName").empty().append("Player: " + gameParams.userName);
+  console.log(gameParams.userName);
+  document.getElementById("playerName").innerHTML= "Jugador: " + gameParams.sessId;
+  //$("#wins").empty().append(gameParams.wins);
+  //$("#losses").empty().append(gameParams.losses);
+  //$("#ties").empty().append(gameParams.stalemates);
+
+}
+
+
+/**
+     * Save Params to cookie.
+     *
+     */
+
+function saveParams() 
+{
+  var cookieStr=gameParams.userName+"|"+gameParams.sessId+"|"+gameParams.wins+"|"+gameParams.losses+"|"+gameParams.stalemates;
+  setCookie("tttGameParams",cookieStr,3);
+}
+
+/**
+     * Get the Game Params from the Cookie.
+     *
+     * @param cookieParams
+     */
+
+function getGameParams(cookieParams) 
+{
+  var parseStr = cookieParams.split("|");
+  gameParams.userName=parseStr[0];
+  gameParams.sessId=parseStr[1];
+  gameParams.wins=parseStr[2];
+  gameParams.losses=parseStr[3];
+  gameParams.stalemates=parseStr[4];
+}
+
+
+    /**
+     * Set the Cookie.
+     *
+     * @param cname
+     * @param cvalue
+     * @param exdays
+     */
+
+function setCookie(cname, cvalue, exdays) 
+{
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+    /**
+     * Get the Cookie from the a specif cookie name.
+     *
+     * @param cname
+     * @returns {string}
+     */
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) 
+  {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1);
+      if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+  }
+  return "";
 }
--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ app.use(express.static('client'));
 var tablero = [[null, null, null], [null, null, null], [null, null, null]];
 var con = 0;
 var jugadores = 0;
+var players=new Array();
 
 io.on('connection', function(socket){
+	setupPlayerAndConnection(socket);
 	console.log('El cliente con IP: ' + socket.handshake.address + 'se ha conectado');
 	jugadores++;
 	io.sockets.emit('jugador', jugadores);
@@ -33,6 +35,86 @@ io.on('connection', function(socket){
     	io.sockets.emit('jugador', jugadores);
   	});
 });
+
+function setupPlayerAndConnection(socket) {
+    //Cookie Process for Name - Ignore Session Id since it most likely is Stale
+    var cookieStr = getCookieValue(socket.request, "tttGameParams");
+    //Load Game Params from Cookie
+    var gameParams = extractParams(cookieStr, socket.id);
+    //Set Player
+    var player = new Player(socket.id, gameParams.userName, gameParams.wins, gameParams.losses, gameParams.stalemate, "new", false);
+    players.push(player);
+    socket.emit('available_games', players);
+    io.emit('player_update', player);
+}
+
+//Gets cookie data from Request and returns Cookie with name
+function getCookieValue(request,cookie) {
+    var list = {},
+        rc = request.headers.cookie;
+    var match="";
+    rc && rc.split(';').forEach(function( cookie ) {
+
+       var parts = cookie.split('=');
+
+       if (parts[0].trim()=="tttGameParams") match = parts[1];
+//        list[parts.shift().trim()] = unescape(parts.join('='));
+    });
+
+    return match;
+}
+
+//Player Object
+function Player(clientId,userName,wins,losses,stalemate,state,ai) {
+
+        this.id=clientId;
+        this.state=state;
+        if (userName !== undefined) {
+            this.playerName=userName;
+        }else
+        {
+            this.playerName=this.id;
+        }
+        this.wins=wins;
+        this.losses=losses;
+        this.stalemate=stalemate;
+       // this.icon=playerIcon;
+        this.computerai=ai;
+        return this;
+}
+
+
+//Game functions
+//Extract User Game details from Cookie
+function extractParams(cookieParams,socketId) {
+    //console.log(cookieParams);
+    var gameParams;
+    if (cookieParams === "") {
+        return {
+            userName:socketId,
+            sessId:socketId,
+            wins:0,
+            losses:0,
+            stalemates:0
+
+        };
+    }else
+    {
+        var parseStr = cookieParams.split("|");
+        return {
+            userName:parseStr[0],
+            sessId:parseStr[1],
+            wins:parseStr[2],
+            losses:parseStr[3],
+            stalemates:parseStr[4]
+        };
+
+    }
+
+
+
+
+}
 
 server.listen(6677, function(){
 	console.log('Servidor está funcionando en http://localhost:6677');
